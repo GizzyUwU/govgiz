5d31b6886184dfe06dc056f245a6f25fb457a999
gizzy <nerop455@gmail.com>
2026-03-15 15:31:52 +0000

    chore: remove log cuz it does work now?

--- src/app.tsx
+++ src/app.tsx
@@ -40,7 +40,6 @@ export default function App() {
     const currentHostname = new URL(
       document.referrer || document.location.href,
     ).hostname.toLowerCase();
-    console.log(currentHostname)
     let index = data.findIndex(
       (m) => new URL(m.url).hostname.toLowerCase() === currentHostname,
     );
